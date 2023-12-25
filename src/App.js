24023aff31ed5f1bcbf752770c6ba443b7637c53
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2023-12-25 16:27:21 +0300

    добавил руты

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, useLocation} from 'react-router-dom';
 import Menu from './components/Menu';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
 import ContactPage from './components/ContactPage';
 import Footer from './components/Footer'
 import ChatPage from "./components/ChatPage";
-import ImageClassificationPage from "./components/ImagePage";
-import SpeechToTextPage from "./components/AudioPage";
+import AudioPage from "./components/AudioPage";
+import ImagePage from "./components/ImagePage";
+function App() {
 const LocationAwareFooter = () => {
     const location = useLocation();
 
-    if (location.pathname === '/chat') {
+    if (location.pathname === '/chat' || location.pathname === '/image' || location.pathname === '/audio') {
+
         return null;
     }
 
     return <Footer />;
 };
 
-function App() {
-    return (
-        <Router>
-            <div id="root">
-                <Menu />
-                <div className="content">
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/about" element={<AboutPage />} />
-                        <Route path="/contact" element={<ContactPage />} />
-                        <Route path="/chat" element={<ChatPage />} />
-                        <Route path="/image" element={<ImageClassificationPage />} />
-                        <Route path="/audio" element={<SpeechToTextPage />} />
-                        {/* Определите другие маршруты здесь */}
-                    </Routes>
-                </div>
-                <LocationAwareFooter />
-            </div>
-        </Router>
-    );
+return (
+  <Router>
+      <div id="root">
+          <Menu />
+          <div className="content">
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/about" element={<AboutPage />} />
+                  <Route path="/contact" element={<ContactPage />} />
+                  <Route path="/chat" element={<ChatPage />} />
+                  <Route path="/audio" element={<AudioPage/>}/>
+                  <Route path="/image" element={<ImagePage/>}/>
+                  {/* Define other routes here */}
+              </Routes>
+          </div>
+          <LocationAwareFooter />
+      </div>
+  </Router>
+);
 }
 
 export default App;
