b619d9043e5204768b269995b4a87b2f629b2399
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2024-03-08 16:36:10 +0300

    измененил хедер в моделях

--- src/components/TestingPage.js
+++ src/components/TestingPage.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, {useState, useEffect, useMemo, useCallback} from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import queryString from 'query-string';
 import {
     AppBar,
     Toolbar,
@@ -12,27 +11,52 @@ import {
     Grid,
     Menu,
     MenuItem,
-    Pagination,
-    InputBase
+    Pagination
 } from '@mui/material';
 import SortIcon from '@mui/icons-material/Sort';
 import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
 import axios from 'axios';
 import ModelCard from "./ModelCard";
 import LogoLink from "./LogoLink";
+import { debounce } from 'lodash';
+
+const useUrlParams = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const processUrlParams = (search) => {
+        const urlParams = new URLSearchParams(search);
+        let paramsFromUrl = Object.fromEntries(urlParams.entries());
+
+        return {
+            pipelineId: paramsFromUrl.pipelineId || 'all',
+            sort: paramsFromUrl.sort || 'trending',
+            page: parseInt(paramsFromUrl.page, 10) || 1,
+            searchQuery: paramsFromUrl.searchQuery || ''
+        };
+    };
+
+    const [params, setParams] = useState(() => processUrlParams(location.search));
+
+    useEffect(() => {
+        setParams(processUrlParams(location.search));
+    }, [location.search]);
+
+    const updateParams = (newParams) => {
+        const updatedParams = { ...params, ...newParams };
+        setParams(updatedParams);
+        const searchParams = new URLSearchParams(updatedParams).toString();
+        navigate(`?${searchParams}`);
+    };
+
+    return [params, updateParams];
+};
 
 const MainLayout = () => {
     // eslint-disable-next-line no-undef
     const baseUrl = process.env.REACT_APP_API_BASE_URL;
-    const navigate = useNavigate();
-    const location = useLocation();
 
-    const [parameters, setParameters] = useState({
-        pipelineId: null,
-        sort: 'trending',
-        page: 1,
-        searchQuery: ''
-    });
     const [pipelines, setPipelines] = useState([]);
     const [models, setModels] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
@@ -40,46 +64,29 @@ const MainLayout = () => {
     const [sortAnchorEl, setSortAnchorEl] = useState(null);
     const [selectedPipelineId, setSelectedPipelineId] = useState(null);
 
+    const [parameters, setParameters] = useUrlParams({
+        pipelineId: 'all',
+        sort: 'trending',
+        page: 1,
+        searchQuery: ''
+    });
+
+    const [inputValue, setInputValue] = useState(parameters.searchQuery);
+
     useEffect(() => {
         fetchPipelines();
     }, []);
 
     useEffect(() => {
-        const params = queryString.parse(location.search);
-        const updatedParameters = {
-            pipelineId: params.pipeline || null,
-            page: parseInt(params.page, 10) || 1,
-            searchQuery: params.search || '',
-            sort: params.sort || 'trending'
-        };
-        if (JSON.stringify(parameters) !== JSON.stringify(updatedParameters)) {
-            setParameters(updatedParameters);
-        }
-    }, [location.search]);
-
-    useEffect(() => {
         fetchModels();
-    }, [location.search]);
+    }, [parameters.pipelineId, parameters.sort, parameters.page, parameters.searchQuery]);
 
-    useEffect(() => {
-        const query = queryString.stringify({
-            page: parameters.page,
-            search: encodeURIComponent(parameters.searchQuery),
-            sort: parameters.sort,
-            pipeline: parameters.pipelineId
-        });
-        const newSearch = `?${query}`;
-        if (location.search !== newSearch) {
-            navigate(newSearch);
-        }
-    }, [parameters]);
-
-    const fetchPipelines = useCallback(async () => {
+    const fetchPipelines = async () => {
         const response = await axios.get(`${baseUrl}/api/v1/pipelines`);
         setPipelines(response.data);
-    }, [baseUrl]);
+    };
 
-    const fetchModels = useCallback(async () => {
+    const fetchModels = async () => {
         const { pipelineId, sort, page, searchQuery } = parameters;
         let url = `${baseUrl}/api/v1/models?sort=${sort}&p=${page}`;
         if (pipelineId) url += `&pipeline=${pipelineId}`;
@@ -89,20 +96,32 @@ const MainLayout = () => {
         setModels(response.data.models);
         setNumTotalItems(response.data.numTotalItems);
         setTotalPages(Math.ceil(response.data.numTotalItems / response.data.numItemsPerPage));
-    }, [parameters, baseUrl]);
+    };
+
+    const debouncedSetSearchQuery = useCallback(
+        debounce((newQuery) => {
+            setParameters({ searchQuery: newQuery, page: 1 });
+        }, 300),
+        []
+    );
+
+    useEffect(() => {
+        setInputValue(parameters.searchQuery);
+    }, [parameters.searchQuery]);
 
     const handleSearchChange = (event) => {
-        setParameters(prev => ({ ...prev, searchQuery: event.target.value }));
+        setInputValue(event.target.value);
+        debouncedSetSearchQuery(event.target.value);
     };
 
     const handleSearchSubmit = (event) => {
         event.preventDefault();
-        fetchModels();
+        setParameters({ searchQuery: parameters.searchQuery, page: 1 });
     };
 
     const handlePipelineClick = (pipelineId) => {
-        setSelectedPipelineId(pipelineId);
-        setParameters(prev => ({ ...prev, pipelineId, page: 1 }));
+        setParameters({ pipelineId: pipelineId, page: 1 });
+        setSelectedPipelineId(pipelineId)
     };
 
     const handleSortMenuClick = (event) => {
@@ -112,24 +131,19 @@ const MainLayout = () => {
     const handleSortMenuClose = (sortOption) => {
         setSortAnchorEl(null);
         if (sortOption) {
-            setParameters(prev => ({ ...prev, sort: sortOption, page: 1, searchQuery: '' }));
+            setParameters({ sort: sortOption, page: 1 });
         }
     };
 
     const handlePageChange = (event, value) => {
-        setParameters(prev => ({ ...prev, page: value }));
+        setParameters({ page: value });
     };
 
-    const generateTabElements = () => {
+    const generateTabElements = useMemo(() => {
         return pipelines.map((pipeline) => (
-          <Tab
-            label={pipeline.label}
-            key={pipeline.id}
-            value = {pipeline.id}
-            onClick={() => handlePipelineClick(pipeline.id)}
-          />
+            <Tab label={pipeline.label} key={pipeline.id} value={pipeline.id} sx={{ color: 'white' }} onClick={() => handlePipelineClick(pipeline.id)} />
         ));
-    };
+    }, [pipelines]);
 
     return (
       <Box sx={{ display: 'flex', height: '100vh' }}>
@@ -139,31 +153,33 @@ const MainLayout = () => {
                   <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
                       Models: {numTotalItems}
                   </Typography>
-              </Toolbar>
-          </AppBar>
-          <Box component="main" sx={{ flexGrow: 1, p: 3, paddingTop: '100px' }}>
-              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                   <Tabs
                     value={selectedPipelineId}
                     variant="scrollable"
                     scrollButtons="auto"
                     allowScrollButtonsMobile
+                    sx={{
+                        '& .Mui-selected': {
+                            backgroundColor: 'indigo',
+                            color: 'white',
+                        },
+                    }}
                   >
-                      {generateTabElements()}
+                      {generateTabElements}
                   </Tabs>
-                  <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                  <Box component="form" onSubmit={handleSearchSubmit} sx={{ display: 'flex', alignItems: 'center', backgroundColor: 'white', borderRadius: 1, p: 0.5 }}>
-                      <InputBase
-                        placeholder="Filter by name…"
-                        inputProps={{ 'aria-label': 'filter by name' }}
-                        value={parameters.searchQuery}
-                        onChange={handleSearchChange}
-                        sx={{ color: 'black', ml: 1, flex: 1, boxShadow:2, px:2 }}
-                      />
-                      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
-                          <SearchIcon />
-                      </IconButton>
-                  </Box>
+                  <Box sx={{ display: 'flex', alignItems: 'center', ml: 'auto',paddingLeft: '20px' }}>
+                      <Box component="form" onSubmit={handleSearchSubmit} sx={{ display: 'flex', alignItems: 'center', backgroundColor: 'white', borderRadius: 1, p: 0.5 }}>
+                          <InputBase
+                            placeholder="Filter by name…"
+                            inputProps={{ 'aria-label': 'filter by name' }}
+                            value={inputValue}
+                            onChange={handleSearchChange}
+                            sx={{ color: 'black', ml: 1, flex: 1 }}
+                          />
+                          <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                              <SearchIcon />
+                          </IconButton>
+                      </Box>
                       <IconButton
                         size="large"
                         edge="end"
@@ -176,19 +192,21 @@ const MainLayout = () => {
                           <SortIcon/>
                       </IconButton>
                   </Box>
-              </Box>
-              <Menu
-                id="sort-menu"
-                anchorEl={sortAnchorEl}
-                open={Boolean(sortAnchorEl)}
-                onClose={() => handleSortMenuClose()}
-              >
-                  <MenuItem onClick={() => handleSortMenuClose('trending')}>Trending</MenuItem>
-                  <MenuItem onClick={() => handleSortMenuClose('likes')}>Most likes</MenuItem>
-                  <MenuItem onClick={() => handleSortMenuClose('downloads')}>Most downloads</MenuItem>
-                  <MenuItem onClick={() => handleSortMenuClose('created')}>Recently created</MenuItem>
-                  <MenuItem onClick={() => handleSortMenuClose('updated')}>Recently updated</MenuItem>
-              </Menu>
+                  <Menu
+                    id="sort-menu"
+                    anchorEl={sortAnchorEl}
+                    open={Boolean(sortAnchorEl)}
+                    onClose={() => handleSortMenuClose()}
+                  >
+                      <MenuItem onClick={() => handleSortMenuClose('trending')}>Trending</MenuItem>
+                      <MenuItem onClick={() => handleSortMenuClose('likes')}>Most likes</MenuItem>
+                      <MenuItem onClick={() => handleSortMenuClose('downloads')}>Most downloads</MenuItem>
+                      <MenuItem onClick={() => handleSortMenuClose('created')}>Recently created</MenuItem>
+                      <MenuItem onClick={() => handleSortMenuClose('updated')}>Recently updated</MenuItem>
+                  </Menu>
+              </Toolbar>
+          </AppBar>
+          <Box component="main" sx={{ flexGrow: 1, p: 3, paddingTop: '120px' }}>
               <Grid container spacing={2}>
                   {models.map((model) => (
                     <Grid item xs={12} sm={6} md={4} key={model.id}>
@@ -196,34 +214,34 @@ const MainLayout = () => {
                     </Grid>
                   ))}
               </Grid>
-              <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+              <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: 4, backgroundColor: '#fff' }}>
                   <Pagination
                     count={totalPages}
                     page={parameters.page}
                     onChange={handlePageChange}
-                    color="primary"
+                    color={"standard"}
                     sx={{
                         '& .MuiPaginationItem-root': {
-                            color: 'primary.main',
+                            color: 'white',
                         },
                         '& .Mui-selected': {
                             backgroundColor: 'primary.main',
                             color: 'white',
                         },
                         '& .MuiPaginationItem-ellipsis': {
-                            color: 'primary.main',
+                            color: 'white',
                         },
                         '& .MuiPaginationItem-page': {
-                            color: 'primary.main',
+                            color: 'white',
                         },
                         borderRadius: '4px',
-                        padding: '10px'
+                        padding: '10px',
                     }}
                   />
               </Box>
           </Box>
       </Box>
     );
-};
+}
 
 export default MainLayout;
