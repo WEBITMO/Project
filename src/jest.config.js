9b1e9b13dcdaa17a263aa843d791fae97185d9cc
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2024-03-09 00:14:56 +0300

    обновил

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,4 +1,7 @@
-export default {
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': 'babel-jest',
+  },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   testEnvironment: 'jsdom',
 };
